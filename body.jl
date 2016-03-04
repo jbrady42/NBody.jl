dc766a1a236f5a4828e7cac5b433f9996a06d9d5
JB <jabrady42@gmail.com>
2016-03-04 07:51:16 -0600

    Refactor

--- body.jl
+++ body.jl
@@ -5,8 +5,9 @@ type Body
 	mass::Float64
 	pos::Vector{Float64}
 	vel::Vector{Float64}
+	e0
 
-	Body(mass=0.0, pos=Vector{Float64}(3), vel=Vector{Float64}(3)) = new(mass, pos, vel)	
+	Body(mass=0.0, pos=Vector{Float64}(3), vel=Vector{Float64}(3)) = new(mass, pos, vel, 0)	
 end
 
 import Base.show
@@ -31,6 +32,78 @@ function read_body()
 	pos = map(x -> parse(Float64, x), split(readline()))
 	vel = map(x -> parse(Float64, x), split(readline()))
 	return Body(mass, pos, vel)
+end
+
+function write_stats(body::Body, steps, time)
+	tot_energy = total_energy(body)
+	s = """
+	Time: $(@sprintf("%.3g", time)) , steps: $steps
+	E_Kin: $(@sprintf("%.3g", kin_energy(body)))
+	E_Pot: $(@sprintf("%.3g", pot_energy(body)))
+	E_Tot: $(@sprintf("%.3g", total_energy(body)))
+	E_Tot - E_init: $(@sprintf("%.3g", (tot_energy - body.e0)))
+	(E_tot - E_init) / E_init: $(@sprintf("%.3g", (tot_energy - body.e0) / body.e0))
+
+	"""
+	write(STDERR, s)
+end
+
+####### Energy #########
+
+function kin_energy(body::Body)
+	return 0.5 * dot(body.vel, body.vel)
+end
+
+function pot_energy(body::Body)
+	return -body.mass / sqrt(dot(body.pos, body.pos))
+end
+
+function total_energy(body::Body)
+	return kin_energy(body) + pot_energy(body)
+end
+
+function init_energy!(body::Body)
+	body.e0 = total_energy(body)
+end
+
+###### Integrators #####
+
+function evolve(body::Body, dt, time_end, dt_output, dt_stats)
+	current_time = 0
+	current_step = 0
+	# num_steps = round(Int, time_units / dt)
+
+	t_stats = dt_stats - 0.5*dt
+	t_checkp = dt_output - 0.5*dt
+	t_end = time_end - 0.5*dt
+
+	init_energy!(body)
+	write_stats(body, current_step, current_time)
+
+	while current_time < t_end
+		integration_step(body, dt)
+		current_time += dt
+		current_step += 1
+		if current_time > t_stats
+			write_stats(body, current_step, current_time)
+			t_stats += dt_stats
+		end
+		if current_time > t_checkp
+			print(body)
+			t_checkp += dt_output
+		end
+
+	end
+
+	# show(body)
+end
+
+function integration_step(body::NB.Body, dt)
+	r2 = dot(body.pos, body.pos)
+	r3 = r2 * sqrt(r2) # we want r^(2/3)
+	acc = body.pos * (-body.mass / r3)
+	body.pos += body.vel * dt
+	body.vel += acc * dt
 end
 
 end
--- run.jl
+++ run.jl
@@ -1,23 +1,14 @@
 
 include("body.jl")
 
-# b = NB.Body(1, [0.5, 0, 0], [0, 0.7, 0])
-function simple_integrator()
-	dt = 0.01 	
-	num_steps = 100
-
+function simple_integrator_main(	)
+	dt = 0.00001
+	dt_stats = 2.5
+	dt_output = 2.5
+	time_end = 2.5
+	
 	body = NB.read_body()
-
-	for i in 1:num_steps
-		r1 = mapreduce(x->x^2, +, body.pos)
-		r = r1 * sqrt(r1) # we want r^(2/3)
-		acc = map(x -> -body.mass * x/r, body.pos)
-		body.pos += body.vel * dt
-		body.vel += acc * dt
-		# println(r)
-		# println(acc)
-	end
-	show(body)
+	NB.evolve(body, dt, time_end, dt_output, dt_stats)
 end
 
 function basic_test()
@@ -25,5 +16,5 @@ function basic_test()
 	print(b)
 end
 
-simple_integrator()
+simple_integrator_main()
 # basic_test()
