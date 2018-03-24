ecac63e69808386faae0a10ea262da53fbbdf035
Jason <jabrady42@gmail.com>
2018-03-24 16:21:50 -0500

    Add integrator param

--- cmd.jl
+++ cmd.jl
@@ -1,22 +1,17 @@
 using ArgParse
+using NBody
+
+integrators = Dict(
+  "euler"     => NBody.forward_step,
+  "leapfrog"  => NBody.leapfrog_step,
+  "rk2"       => NBody.rk4_step,
+  "rk4"       => NBody.rk4_step,
+)
 
 function parse_commandline()
   s = ArgParseSettings()
 
   @add_arg_table s begin
-    "--init_out", "-i"
-      help = "Output initial snapshot"
-      action = :store_true
-
-    "--extra_diagnostics", "-x"
-      help = "Extra diagnostics"
-      action = :store_true
-
-    "--softening_length", "-s"
-      help = "Softening ength"
-      arg_type = Float64
-      default = 0.0
-
     "--total_duration", "-t"
       help = "Duration of the integration"
       arg_type = Float64
@@ -36,6 +31,25 @@ function parse_commandline()
       help = "Integration time step"
       arg_type = Float64
       default = 0.001
+
+    "--integrator", "-i"
+      help = "Integration method"
+      arg_type = String
+      default = "rk4"
+      range_tester = x -> haskey(integrators, x)
+
+    "--extra_diagnostics", "-x"
+      help = "Extra diagnostics"
+      action = :store_true
+
+    "--softening_length", "-s"
+      help = "Softening ength"
+      arg_type = Float64
+      default = 0.0
+
+    "--no_init_out", "-n"
+      help = "Don't output initial snapshot"
+      action = :store_false
   end
 
   return parse_args(s)
@@ -48,11 +62,18 @@ function main(args)
   # Should get moved in with rest of args
   nb.soften_len = args["softening_length"]
 
-  method = NBody.rk4_step
+  method = integrators[args["integrator"]]
 
-  ea = EvolveArgs(method, args["softening_length"], args["step_size"],
-    args["total_duration"], args["output_interval"], args["diagnostics_interval"],
-    args["init_out"], args["extra_diagnostics"])
+  ea = EvolveArgs(
+    method,
+    args["softening_length"],
+    args["step_size"],
+    args["total_duration"],
+    args["output_interval"],
+    args["diagnostics_interval"],
+    !args["no_init_out"],
+    args["extra_diagnostics"]
+  )
 
   write_info(args, ea)
 
@@ -63,7 +84,7 @@ function write_info(args, ea)
   println(STDERR, "A simple N-Body code")
 
   d = Dict(
-    "Integration method" 							=> ea.integ_method,
+    "Integration method"              => ea.integ_method,
     "Integration time step"           => ea.dt,
     "Diagnostics output interval"     => ea.dt_stats,
     "Snapshot output interval"        => ea.dt_output,
@@ -77,8 +98,5 @@ end
 
 
 parsed_args = parse_commandline()
-
-# Include after arg handling
-using NBody
 
 main(parsed_args)
